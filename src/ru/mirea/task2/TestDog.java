0baacd005f7b83e1f8203756e332c182eff504df
danveld <levitskydanielius@gmail.com>
2021-09-23 13:23:59 +0300

    Обновил Task2 Исправил TestDog

--- src/ru/mirea/task2/TestDog.java
+++ src/ru/mirea/task2/TestDog.java
@@ -1,14 +1,22 @@
 package ru.mirea.task2;
 
+import java.util.ArrayList;
+
 public class TestDog {
+    protected static ArrayList<Dog> dogarr = new ArrayList<>();
     public static void main(String[] args) {
-        Dog d1 = new Dog("Mike", 2);
-        Dog d2 = new Dog("Helen", 7);
-        Dog d3 = new Dog("Bob");
-        d3.setAge(1);
-        System.out.println(d1);
-        d1.intoHumanAge();
-        d2.intoHumanAge();
-        d3.intoHumanAge();
+
+        putInArr(new Dog("Mike", 2));
+        putInArr(new Dog("Helen", 7));
+        putInArr(new Dog("Bob"));
+        dogarr.get(2).setAge(1);
+        System.out.println(dogarr.get(1));
+        dogarr.get(0).intoHumanAge();
+        dogarr.get(1).intoHumanAge();
+        dogarr.get(2).intoHumanAge();
+    }
+
+    public static void putInArr(Dog d){
+        dogarr.add(d);
     }
 }
