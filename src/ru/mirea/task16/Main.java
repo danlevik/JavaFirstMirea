cb92f1bb4740431ff18a8df8b29699bb313fcfab
danveld <levitskydanielius@gmail.com>
2021-12-01 17:00:56 +0300

    task18 & task19 completed

--- src/ru/mirea/task16/Main.java
+++ src/ru/mirea/task16/Main.java
@@ -37,10 +37,18 @@ public class Main {
             System.out.println("3 - Оформить запись");
 
             Scanner sc = new Scanner(System.in);
-
-            int action = sc.nextInt();
+            int action = 999;
+                try {
+                    action = sc.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Вы ввели не число.");
+                }
 
             switch (action){
+                default:
+                    System.out.println("Некорректный номер команды!\n");
+                    break;
                 case 0:
                     exit = true;
                     break;
@@ -48,72 +56,88 @@ public class Main {
                 case 1:
                     System.out.println("ДЛЯ ОКОНЧАНИЯ ВВОДА ВМЕСТО ИМЕНИ ВВЕДИТЕ 0");
 
-                    System.out.print("Введите имя врача: ");
-                    String name = sc.next();
-
-                    while (!name.equals("0")) {
-
-                        // номер телефона и почта одни у всех, нам они не особо нужны
-                        String phoneNum = "88005553535";
-                        String email = "doctormail@mail.ru";
-
-                        System.out.println("Введите номер специальности врача из вариантов: ");
-                        System.out.println("1 - Терапевт\n2 - Дерматолог\n3 - Педиатр\n" +
-                                "4 - Уролог\n5 - Хирург\n6 - Психиатр");
-                        int speciality_num = sc.nextInt();
-                        Speciality speciality;
-                        switch (speciality_num){
-                            case 2:
-                                speciality = Speciality.valueOf("DERMATOLOGY");
-                                break;
-                            case 3:
-                                speciality = Speciality.valueOf("PEDIATRICS");
-                                break;
-                            case 4:
-                                speciality = Speciality.valueOf("UROLOGY");
-                                break;
-                            case 5:
-                                speciality = Speciality.valueOf("SURGERY");
-                                break;
-                            case 6:
-                                speciality = Speciality.valueOf("PSYCHIATRY");
-                                break;
-
-                            default:
-                                speciality = Speciality.valueOf("THERAPY");
-                                break;
-                        }
-
-                        System.out.print("Введите зарплату: ");
-                        int salary = sc.nextInt();
-
-                        doctors.add(new Doctor(name, phoneNum, email, speciality, salary));
-
-                        System.out.println("Врач " + name + " добавлен в систему!\n");
-
+                    try {
                         System.out.print("Введите имя врача: ");
-                        name = sc.next();
+                        String name = sc.next();
+
+                        while (!name.equals("0")) {
+                            if (name.length() > 20){
+                                throw new Exception("string is too long");
+                            }
+
+                            // номер телефона и почта одни у всех, нам они не особо нужны
+                            String phoneNum = "88005553535";
+                            String email = "doctormail@mail.ru";
+
+                            System.out.println("Введите номер специальности врача из вариантов: ");
+                            System.out.println("1 - Терапевт\n2 - Дерматолог\n3 - Педиатр\n" +
+                                    "4 - Уролог\n5 - Хирург\n6 - Психиатр");
+                            int speciality_num = sc.nextInt();
+                            Speciality speciality;
+                            switch (speciality_num){
+                                case 2:
+                                    speciality = Speciality.valueOf("DERMATOLOGY");
+                                    break;
+                                case 3:
+                                    speciality = Speciality.valueOf("PEDIATRICS");
+                                    break;
+                                case 4:
+                                    speciality = Speciality.valueOf("UROLOGY");
+                                    break;
+                                case 5:
+                                    speciality = Speciality.valueOf("SURGERY");
+                                    break;
+                                case 6:
+                                    speciality = Speciality.valueOf("PSYCHIATRY");
+                                    break;
+
+                                default:
+                                    speciality = Speciality.valueOf("THERAPY");
+                                    break;
+                            }
+
+                            System.out.print("Введите зарплату: ");
+                            int salary = sc.nextInt();
+
+                            doctors.add(new Doctor(name, phoneNum, email, speciality, salary));
+
+                            System.out.println("Врач " + name + " добавлен в систему!\n");
+
+                            System.out.print("Введите имя врача: ");
+                            name = sc.next();
+                        }
+                    }
+                    catch (Exception e){
+                        System.out.println("Слишком длинное имя! Оно не должно превышать 20 символов.");
                     }
                     break;
 
                 case 2:
                     System.out.println("ДЛЯ ОКОНЧАНИЯ ВВОДА ВМЕСТО ИМЕНИ ВВЕДИТЕ 0");
 
-                    System.out.print("Введите имя пациента: ");
-                    name = sc.next();
-                    while (!name.equals("0")) {
-
-                        // номер телефона и почта одни у всех, нам они не особо нужны
-                        String phoneNum = "88886664646";
-                        String email = "patientmail@mail.ru";
-
-                        patients.add(new Patient(name, phoneNum, email));
-
-                        System.out.println("Пациент " + name + " добавлен в систему!\n");
-
+                    try {
                         System.out.print("Введите имя пациента: ");
-                        name = sc.next();
+                        String name = sc.next();
+                        while (!name.equals("0")) {
+                            if (name.length() > 20){
+                                throw new Exception("string is too long");
+                            }
+                            // номер телефона и почта одни у всех, нам они не особо нужны
+                            String phoneNum = "88886664646";
+                            String email = "patientmail@mail.ru";
+
+                            patients.add(new Patient(name, phoneNum, email));
+
+                            System.out.println("Пациент " + name + " добавлен в систему!\n");
+
+                            System.out.print("Введите имя пациента: ");
+                            name = sc.next();
+                        }
                     }
+                    catch (Exception e){
+                        System.out.println("Слишком длинное имя! Оно не должно превышать 20 символов.");
+                    }
+
                     break;
 
                 case 3:
